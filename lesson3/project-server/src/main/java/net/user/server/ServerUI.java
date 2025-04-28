ad5df9a0c52a7d0609b526e777b69254b51cab6e
Archasmiel <archasmiel@gmail.com>
2025-04-28 23:13:22 +0300

    Lesson 3 (part 3)
    Add fx:id from fxml file
    Correction of presentation

--- lesson3/project-server/src/main/java/net/user/server/ServerUI.java
+++ lesson3/project-server/src/main/java/net/user/server/ServerUI.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,6 +15,11 @@ import java.io.IOException;
 public class ServerUI {
 
     public static final String MAIN_UI = "/server.fxml";
+    
+    @FXML public TextArea consoleArea;
+    @FXML public TextField commandField;
+    @FXML public Button sendButton;
+    @FXML public ListView<String> clientList;
 
     public void show(Stage stage) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(MAIN_UI));
@@ -32,7 +41,11 @@ public class ServerUI {
 
     @FXML
     public void initialize() {
-
+        clientList.getItems().addAll(
+            "@illias",
+            "@archasmiel",
+            "@hubris"
+        );
     }
 
 }
